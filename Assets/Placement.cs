458c5f0ab5c41df1ee4b9519addf2d7f6a1f26c0
TelluriumTrioxide <teodor.silfverhjelm@learnet.se>
2020-11-12 18:13:37 +0100

    Början på ett placementskript

---
+++ Assets/Placement.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+using UnityEngine;
+
+public class Placement
+{
+   UnityEngine.Vector3 myInputCoordinate;
+   Touch myTouch;
+   void PlacementLoop(bool aLoopState)
+   {
+      while (aLoopState)
+      {
+         GetFlooredInput();
+         
+      }
+
+   }
+
+   void GetFlooredInput()
+   {
+      myTouch = Input.GetTouch(0);
+      myInputCoordinate = Camera.main.ScreenToWorldPoint(myTouch.position);
+      myInputCoordinate.z = 0f;
+      UnityEngine.Vector3Int.FloorToInt(myInputCoordinate);
+   }
+   void ExitLoop()
+   {
+
+   }
+}
