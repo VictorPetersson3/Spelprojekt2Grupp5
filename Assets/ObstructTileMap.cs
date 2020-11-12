ae7adb49610524d3665791a424a4b78eac4d06da
Andre bruce <Andre.bruce@studerande.thegameassebly.se>
2020-11-12 18:39:34 +0100

    Create new Script Obstruct Tiles

---
+++ Assets/ObstructTileMap.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+
+public class ObstructTileMap : MonoBehaviour
+{
+    [SerializeField]
+    int myWidth = 1;
+    [SerializeField]
+    int myDepth = 1;
+
+    [SerializeField]
+    WorldController myWorldController;
+
+   
+
+    JobHandle myJobHandle;
+    FindTilesJob myFindTilesJob;
+    private void OnValidate()
+    {
+
+        myWorldController = FindObjectOfType<WorldController>();
+    }
+
+    void Start()
+    {
+        Invoke("CreateJob", 0.5f);
+        myJobHandle.Complete();
+
+    }
+
+
+
+
+    void CreateJob()
+    {
+        myFindTilesJob = new FindTilesJob
+        {
+            worldDepth = myWorldController.GetWorldDepth,
+            worldWidth = myWorldController.GetWorldWidth,
+            sizeX = myWidth,
+            sizeZ = myDepth,
+            objectPosition = transform.position
+        };
+        myJobHandle = myFindTilesJob.Schedule();
+
+    }
+    public static Tile[] GetOverLappingTiles(int aXSize, int aZSize, int aWorldDepth, int aWorldWidth, Vector3 aObjectPosition)
+    {
+        int xPos = Mathf.FloorToInt(aObjectPosition.x);
+        int zPos = Mathf.FloorToInt(aObjectPosition.z);
+
+        int xExtents = aXSize / 2;
+        int zExtents = aZSize / 2;
+
+        Vector3 cornerPos = new Vector3(xPos - xExtents, 0, zPos - zExtents);
+
+        int totalSizeX = Mathf.FloorToInt(cornerPos.x) + aXSize;
+        int totalSizeZ = Mathf.FloorToInt(cornerPos.z) + aZSize;
+
+        Tile[] tileInRange = new Tile[aXSize * aZSize];
+        
+        if (cornerPos.x < 0 || cornerPos.z < 0)
+        {
+            Debug.Log("Some Tiles are out of range");
+            return null;
+        }
+        if (totalSizeZ > aWorldDepth || totalSizeX > aWorldWidth)
+        {
+            Debug.Log("Some Tiles are out of range");
+            return null;
+        }
+        int count = 0;
+        int xValue = 0;
+        int zValue = 0;
+
+        for (int x = xPos - xExtents; x < totalSizeX; x++)
+        {
+            for (int z = (zPos - zExtents); z < totalSizeZ; z++)
+            {
+                tileInRange[count] = WorldController.Instance.GetTileAtPosition(x, z);
+               
+                
+                count++;
+                zValue++;
+            }
+            xValue++;
+        }
+       
+        return tileInRange;
+    }
+
+    public struct FindTilesJob : IJob
+    {
+        public int worldDepth;
+        public int worldWidth;
+        public int sizeX;
+        public int sizeZ;
+        public Vector3 objectPosition;
+
+        public void Execute()
+        {
+            Tile[] tileArray = GetOverLappingTiles(sizeX, sizeZ, worldDepth, worldWidth, objectPosition);
+
+            if (tileArray != null)
+            {
+                for (int i = 0; i < tileArray.Length; i++)
+                {
+                    if (tileArray[i].GetSetTileState == Tile.TileState.empty)
+                    {
+                        Tile t = tileArray[i];
+                        t.GetSetTileState = Tile.TileState.obstructed;
+                        tileArray[i] = t;
+
+                    }
+                    WorldController.Instance.GetWorld.CopySetTile(tileArray[i]);
+               
+                  
+                }
+
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(new Vector3(transform.position.x, 0, transform.position.z), new Vector3(myWidth, 0, myDepth));
+    }
+}
--- Assets/WorldController.cs
+++ Assets/WorldController.cs
@@ -7,36 +7,57 @@ public class WorldController : MonoBehaviour
     public static WorldController Instance { get; protected set; }
     [Header("World Size Settings")]
     [SerializeField]
+    [Range(0, 25)]
     int myWidth = 10;
+    [Range(0, 25)]
     [SerializeField]
     int myDepth = 10;
-    
+
     World myWorld;
     public World GetWorld { get { return myWorld; } }
-
+    public int GetWorldDepth { get { return myDepth; } }
+    public int GetWorldWidth { get { return myWidth; } }
     void Start()
     {
-        myWorld = new World(myWidth, myWidth); 
+        myWorld = new World(myWidth, myWidth);
         if (Instance != null)
-        {
             Debug.LogError("There should never be two world controllers.");
-        }        
         Instance = this;
-        //for (int i = 0; i < myWidth; i++)
-        //{
-        //    for (int j = 0; j < myDepth; j++)
-        //    {
-        //        Debug.Log("Tile: " + myWorld.GetTileAt(i, j).GetX + ", " + myWorld.GetTileAt(i, j).GetZ + ", " + myWorld.GetTileAt(i,j).GetSetTileState );
-
-        //    }
-        //}
-        
-    }    
+    }
     public Tile GetTileAtPosition(float aX, float aZ)
     {
         int x = Mathf.FloorToInt(aX);
         int z = Mathf.FloorToInt(aZ);
-        return myWorld.GetTileAt(x, z);
+        return this.myWorld.GetTileAt(x, z);
+    }
+    [ExecuteInEditMode]
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(new Vector3((float)(myWidth / 2.0f), 0, (float)(myDepth / 2.0f)), new Vector3(myWidth, 0.1f, myDepth));
+
+        if (myWorld != null)
+        {
+            for (int i = 0; i < myWidth; i++)
+            {
+                for (int j = 0; j < myDepth; j++)
+                {
+                    if (myWorld.GetTileAt(i, j).GetSetTileState == Tile.TileState.obstructed)
+                    {
+                        Gizmos.color = Color.red;
+                        Gizmos.DrawWireCube(new Vector3(i, 0, j), new Vector3(1,0,1));
+                    }
+
+                }
+            }
+
+        }
+
+
+    }
+    private void Update()
+    {
+
     }
 
 }
