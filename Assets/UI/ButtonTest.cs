181bfb085ee5211f8a08dd75cfb413de883712be
LEARNET\victor.petersson3 <victor.petersson@studerande.thegameassembly.se>
2020-11-16 15:19:30 +0100

    More UI functionality and adding packages

---
+++ Assets/UI/ButtonTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+    public enum aEnumType {temp, s, a };
+
+    
+    
+    public void SelectRail()
+    {
+        
+    }
+    
+}
