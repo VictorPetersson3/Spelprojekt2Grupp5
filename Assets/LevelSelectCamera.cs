42e4893839a5fab6f8c24e1cd36d7f47d1ce724d
TelluriumTrioxide <71002915+TelluriumTrioxide@users.noreply.github.com>
2020-11-23 18:10:46 +0100

    Påbörjat level select scen

---
+++ Assets/LevelSelectCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectCamera : MonoBehaviour
+{
+   private GameObject myObjectInFocus;
+   [SerializeField] GameObject myLevelSelect;
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelect : MonoBehaviour
+{
+   [SerializeField] GameObject myWorld;
+   private GameObject mySelectedLevel;
+   void Update()
+   {
+      if (Input.GetMouseButtonDown(0))
+      {
+         mySelectedLevel = WhatDidIHit();
+         Debug.Log(mySelectedLevel);
+      }
+   }
+   public GameObject WhatDidIHit()
+   {
+      Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+      RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+      if (hit)
+      {
+         Debug.Log("Funkar");
+         return hit.collider.gameObject;
+      }
+      else
+      {
+         Debug.Log("Funkar inte");
+         return myWorld;
+      }
+   }
+}
