53fb51b09c5ab56244ddddc031b6ee86980cfee8
TelluriumTrioxide <71002915+TelluriumTrioxide@users.noreply.github.com>
2020-11-24 18:04:40 +0100

    Början på level select camera

--- Assets/LevelSelectCamera.cs
+++ Assets/LevelSelectCamera.cs
@@ -4,10 +4,60 @@ using UnityEngine;
 
 public class LevelSelectCamera : MonoBehaviour
 {
-   private GameObject myObjectInFocus;
-   [SerializeField] GameObject myLevelSelect;
-    void Update()
-    {
-        
-    }
+   [SerializeField] private float transitionSpeed = 5f;
+
+   private float myZoomPaddingTopDown = 22f;
+   private float myCurrentZoom = 10f;
+
+   private Vector3 myWorldCenterPostion = Vector3.zero;
+   private Vector3 myTouchStart;
+   private Vector3 myFocusOffset = Vector3.zero;
+   [SerializeField] private Vector3 myFocusPosition = Vector3.zero;
+   [SerializeField] private Quaternion myFocusRotation = Quaternion.identity;
+
+   private GameObject myObjectInFocus = null;
+
+   private void Start()
+   {
+      myWorldCenterPostion = transform.position;
+      myFocusOffset = myFocusPosition;
+   }
+   void Update()
+   {
+      if (myObjectInFocus != null)
+      {
+         myFocusPosition = myObjectInFocus.transform.position + myFocusOffset;
+         transform.position = Vector3.Lerp(transform.position, myFocusPosition, Time.deltaTime * transitionSpeed);
+         transform.rotation = Quaternion.Lerp(transform.rotation, myFocusRotation, Time.deltaTime * transitionSpeed);
+      }
+      else
+      {
+         myFocusPosition = myFocusOffset;
+         transform.position = Vector3.Lerp(transform.position, new Vector3(myWorldCenterPostion.x, myCurrentZoom, myWorldCenterPostion.z), Time.deltaTime * transitionSpeed);
+         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(75, 0, 0), Time.deltaTime * transitionSpeed);
+         Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, myZoomPaddingTopDown, Time.deltaTime * transitionSpeed);
+      }
+      //if (Input.GetMouseButtonDown(0))
+      //{
+      //   myTouchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+      //}
+
+      //else if (Input.GetMouseButton(0))
+      //{
+      //   Vector3 direction = myTouchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+      //   myWorldCenterPostion += direction;
+      //}
+   }
+   public void SetFocus(GameObject aGameObject)
+   {
+      myObjectInFocus = aGameObject;
+   }
+   public void Pan(Vector3 aPosition)
+   {
+      myWorldCenterPostion += aPosition;
+   }
+   public void Zoom()
+   {
+
+   }
 }
--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -4,29 +4,24 @@ using UnityEngine;
 
 public class LevelSelect : MonoBehaviour
 {
-   [SerializeField] GameObject myWorld;
    private GameObject mySelectedLevel;
    void Update()
    {
-      if (Input.GetMouseButtonDown(0))
+      if (Input.GetMouseButtonDown(0) || Input.touchCount == 1)
       {
          mySelectedLevel = WhatDidIHit();
-         Debug.Log(mySelectedLevel);
+         Camera.main.gameObject.GetComponent<LevelSelectCamera>().SetFocus(mySelectedLevel);
       }
    }
    public GameObject WhatDidIHit()
    {
-      Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
-      RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
-      if (hit)
+      RaycastHit hit;
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      if (Physics.Raycast(ray, out hit))
       {
-         Debug.Log("Funkar");
+         Debug.Log(hit.collider.gameObject);
          return hit.collider.gameObject;
       }
-      else
-      {
-         Debug.Log("Funkar inte");
-         return myWorld;
-      }
+      return null;
    }
 }
