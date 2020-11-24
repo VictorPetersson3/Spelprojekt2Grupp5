24c9aac19f1487f5867f07d5cb693d04b0d3e2a6
Andre bruce <Andre.bruce@studerande.thegameassebly.se>
2020-11-24 13:24:34 +0100

    fixed issue wtith github

--- Assets/Scripts/PathTile.cs
+++ Assets/Scripts/PathTile.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class PathTile : MonoBehaviour
 {
+    [SerializeField]
+    bool isEndTile = false;
+
     Vector3 myPosition;
-    public Vector3 GetPathTilePosition { get{ return myPosition; }set { myPosition = value; } }
+    public Vector3 GetPathTilePosition { get { return myPosition; } set { myPosition = value; } }
+    public bool IsEndTile { get { return isEndTile; } }
+
+    void Start()
+    {
+        myPosition = new Vector3(Mathf.FloorToInt(transform.position.x), 0, Mathf.FloorToInt(transform.position.z));
+        transform.position = myPosition;
+    }
 
 }
  
