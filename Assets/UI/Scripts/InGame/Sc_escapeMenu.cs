f1a6a73c37e155e3e80f24028cf56258cc92708b
LEARNET\victor.petersson3 <victor.petersson@studerande.thegameassembly.se>
2020-11-25 16:56:30 +0100

    End of Game Screen and plenty of UI tweaks

--- Assets/UI/Scripts/InGame/Sc_escapeMenu.cs
+++ Assets/UI/Scripts/InGame/Sc_escapeMenu.cs
@@ -16,15 +16,36 @@ public class Sc_escapeMenu : MonoBehaviour
 
     public void EscapeMenuUp(GameObject aEscapeMenuContainer)
     {
-        myEscapeMenuDownArrow.SetActive(true);
-        myEscapeMenuUpArrow.SetActive(false);
-        LeanTween.move(myEscapeMenuRect, myMin, 1.0f).setEase(easeType);
+        if (!LeanTween.isTweening(myEscapeMenuRect))
+        {
+            Invoke("DeActivateUpArrow", 0.5f);
+            Invoke("MoveMenuUp", 0.1f);
+        }
     }
     public void EscapeMenuDown(GameObject aEscapeMenuContainer)
     {
-        LeanTween.move(myEscapeMenuRect, new Vector3(0, -520.0f, 0), 1.0f).setEase(easeType);
+        if (!LeanTween.isTweening(myEscapeMenuRect))
+        {
+            Invoke("ActivateUpArrow", 0.5f);
+            Invoke("MoveMenuDown", 0.1f);
+        }
+    }
+    void ActivateUpArrow()
+    {
         myEscapeMenuUpArrow.SetActive(true);
         myEscapeMenuDownArrow.SetActive(false);
     }
-
+    void DeActivateUpArrow()
+    {
+        myEscapeMenuDownArrow.SetActive(true);
+        myEscapeMenuUpArrow.SetActive(false);
+    }
+    void MoveMenuUp()
+    {
+        LeanTween.move(myEscapeMenuRect, myMin, 1.0f).setEase(easeType);
+    }
+    void MoveMenuDown()
+    {
+        LeanTween.move(myEscapeMenuRect, new Vector3(0, -520.0f, 0), 1.0f).setEase(easeType);
+    }
 }
