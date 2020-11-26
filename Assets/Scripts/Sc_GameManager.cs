6550d2dee134e0e6a4cd73d6ca78ee8197715ca1
LEARNET\victor.petersson3 <victor.petersson@studerande.thegameassembly.se>
2020-11-26 18:45:13 +0100

    Made the level manager more managing

---
+++ Assets/Scripts/Sc_GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Sc_GameManager : MonoBehaviour
+{
+
+    public static Sc_GameManager myInstance;
+    public GameObject myLoadingScreen;
+
+    private void Awake()
+    {
+        myInstance = this;
+
+        SceneManager.LoadSceneAsync((int)Sc_SceneIndexes.eSceneIndexes.eMainMenu, LoadSceneMode.Additive);
+    }
+
+    public void LoadGame()
+    {
+        myLoadingScreen.gameObject.SetActive(true);
+        SceneManager.UnloadSceneAsync((int)Sc_SceneIndexes.eSceneIndexes.eMainMenu);
+        Invoke("DelaySpawn", 5.0f);
+    }
+    void DelaySpawn()
+    {
+        SceneManager.LoadSceneAsync((int)Sc_SceneIndexes.eSceneIndexes.eLevel01, LoadSceneMode.Additive);
+    }
+
+}
