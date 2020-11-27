09b62983e23535a55e212f95bc6c023dcc1a760d
TelluriumTrioxide <71002915+TelluriumTrioxide@users.noreply.github.com>
2020-11-27 20:07:47 +0100

    Fixade linjer mellan level-objekt

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class LevelSelect : MonoBehaviour
 {
+   [Header("Parent GameObject till alla level-objekt in här")]
+   [SerializeField] private GameObject myLevels;
+
+   [Header("Linjen mellan levlarnas tjocklek")]
+   [SerializeField] private GameObject myLine;
+   [SerializeField] private Vector2 myLineThickness = new Vector2(0.1f, 0.1f);
+
+   private GameObject[] myLineList;
    private GameObject mySelectedLevel;
+
+   private void Start()
+   {
+      GenerateLines();
+   }
    void Update()
    {
-      if (Input.GetMouseButtonDown(0) && Input.touchCount == 1)
+      if (Input.GetMouseButtonDown(0) && Input.touchCount <= 1)
       {
          mySelectedLevel = WhatDidIHit();
          Camera.main.gameObject.GetComponent<LevelSelectCamera>().SetFocus(mySelectedLevel);
@@ -23,5 +36,64 @@ public class LevelSelect : MonoBehaviour
          return hit.collider.gameObject;
       }
       return null;
+   }
+   private void GenerateLines()
+   {
+      Transform[] levelTransforms = new Transform[myLevels.gameObject.transform.childCount];
+      Vector3 linePos;
+      Vector3 lineScale = Vector3.zero;
+      Quaternion lineRot = Quaternion.identity;
+      myLineList = new GameObject[myLevels.gameObject.transform.childCount];
+
+      Debug.Log(myLevels.gameObject.transform.GetChild(0));
+      Debug.Log(myLevels.gameObject.transform.GetChild(1));
+
+      levelTransforms[0] = myLevels.gameObject.transform.GetChild(0);
+
+      for (int i = 1; i < myLevels.gameObject.transform.childCount; i++)
+      {
+         levelTransforms[i] = myLevels.gameObject.transform.GetChild(i);
+
+         linePos = levelTransforms[i - 1].position + (levelTransforms[i].position - levelTransforms[i - 1].position) / 2;
+
+
+         lineScale.x = Mathf.Sqrt
+         (
+            Mathf.Pow
+            (
+               levelTransforms[i].position.x - levelTransforms[i - 1].position.x, 
+               2
+            )+
+            Mathf.Pow
+            (
+               levelTransforms[i].position.y - levelTransforms[i - 1].position.y, 
+               2
+            )+ 
+            Mathf.Pow
+            (
+               levelTransforms[i].position.z - levelTransforms[i - 1].position.z, 
+               2
+            ) 
+         );
+         lineScale.y = myLineThickness.x;
+         lineScale.z = myLineThickness.y;
+
+         lineRot = Quaternion.Euler
+         (
+            0, 
+            -Mathf.Atan
+            (
+               (levelTransforms[i].position.z - levelTransforms[i - 1].position.z) / (levelTransforms[i].position.x - levelTransforms[i - 1].position.x)
+            ) * 180 / Mathf.PI, 
+            -Mathf.Atan
+            (
+               (levelTransforms[i].position.y - levelTransforms[i - 1].position.y) / (levelTransforms[i].position.x - levelTransforms[i - 1].position.x)
+            ) * 180 / Mathf.PI
+         );
+
+         myLineList[i - 1] = Instantiate(myLine, linePos, lineRot, levelTransforms[i - 1]);
+         myLineList[i - 1].transform.localScale = lineScale;
+         myLineList[i - 1].SetActive(true);
+      }
    }
 }
