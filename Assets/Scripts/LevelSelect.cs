4f25d6f20f137a677726681baaa453fc5c8db95e
TelluriumTrioxide <71002915+TelluriumTrioxide@users.noreply.github.com>
2020-12-01 16:51:52 +0100

    Stjärnor och grejer

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class LevelSelect : MonoBehaviour
 {
-//   [SerializeField] private GameObject[] myLevels;
-
+   [SerializeField] private GameObject myMainUI;
    [Header("Linjen mellan levlarnas tjocklek")]
    [SerializeField] private GameObject myLine;
    [SerializeField] private Vector2 myLineThickness = new Vector2(0.1f, 0.1f);
@@ -13,23 +12,19 @@ public class LevelSelect : MonoBehaviour
    private GameObject[] myLineList;
    private GameObject mySelectedLevel;
    private GameObject mySelectedUI;
-   [SerializeField] private GameObject myMainUI;
+   private int myCurrentStars;
+   [SerializeField] private int[] myStarRequirement;
 
-   //private void OnValidate()
-   //{
-   //   myLevels = new GameObject[transform.childCount];
-   //   for (int i = 0; i < transform.childCount; i++)
-   //   {
-   //      myLevels[i] = transform.GetChild(i).gameObject.transform.GetChild(i).transform.gameObject;
-   //   }
-   //}
+  
    private void Start()
    {
+      Unlock(myCurrentStars);
       GenerateLines();
       mySelectedUI = myMainUI;
    }
    void Update()
    {
+
       if (Input.GetMouseButtonDown(0) && Input.touchCount <= 1)
       {
          mySelectedLevel = WhatDidIHit();
@@ -54,7 +49,6 @@ public class LevelSelect : MonoBehaviour
       Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
       if (Physics.Raycast(ray, out hit))
       {
-         Debug.Log(hit.collider.gameObject);
          return hit.collider.gameObject;
       }
       return null;
@@ -67,11 +61,11 @@ public class LevelSelect : MonoBehaviour
       Quaternion lineRot;
       myLineList = new GameObject[transform.childCount];
 
-      levelTransforms[0] = transform.GetChild(0).GetChild(1);
+      levelTransforms[0] = transform.GetChild(0).GetChild(2);
 
       for (int i = 1; i < transform.childCount; i++)
       {
-         levelTransforms[i] = transform.GetChild(i).GetChild(1);
+         levelTransforms[i] = transform.GetChild(i).GetChild(2);
 
          linePos = levelTransforms[i - 1].position + (levelTransforms[i].position - levelTransforms[i - 1].position) / 2;
 
@@ -119,6 +113,19 @@ public class LevelSelect : MonoBehaviour
          myLineList[i - 1] = Instantiate(myLine, linePos, lineRot, levelTransforms[i - 1]);
          myLineList[i - 1].transform.localScale = lineScale;
          myLineList[i - 1].SetActive(true);
+      }
+   }
+   private void Unlock(int anAmountOfStars)
+   {
+      for (int i = 0; i < transform.childCount; i++)
+      {
+         if (!transform.GetChild(i).GetChild(1).gameObject.activeSelf)
+         {
+            if (anAmountOfStars >= myStarRequirement[i])
+            {
+               transform.GetChild(i).GetChild(1).gameObject.SetActive(true);
+            }
+         }
       }
    }
 }
