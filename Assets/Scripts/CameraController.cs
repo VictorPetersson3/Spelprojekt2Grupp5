6e6f6f75ef198e461e677f257aadd020623587f1
Oliwer Dahlin <oliwerdahlin@gmail.com>
2020-12-03 13:55:52 +0100

    More camera stuff

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
@@ -15,17 +16,25 @@ public class CameraController : MonoBehaviour
 
     public Renderer[] myTargets;
 
+    public float targetCameraWidth = 35f;
+    public Text debugText;
+
     [SerializeField]
     float myZoomPaddingPortrait = -20f;
     [SerializeField]
     float myZoomPaddingLandscape = 2f;
     [SerializeField]
-    float myZoomPaddingTopDown = 22f;
+    float myZoomPaddingTopDownPortrait = 22f;
+    [SerializeField]
+    float myZoomPaddingTopDownLandscape = 22f;
     [SerializeField]
     float transitionSpeed = 5f;
     [SerializeField]
     Vector3 myWorldCenterPostion = Vector3.zero;
 
+    Plane[] frustumPlanes;
+
+    float startingCameraWidth;
 
     float currentPadding;
 
@@ -38,32 +47,53 @@ public class CameraController : MonoBehaviour
     {
         originalPosition = transform.position;
         originalRotation = transform.rotation;
+
+        targetCameraWidth = (Mathf.Abs(Mathf.Sqrt(Mathf.Pow(myTargets[0].bounds.size.x, 2) + Mathf.Pow(myTargets[0].bounds.size.z, 2))) / 1.86f);
+        startingCameraWidth = targetCameraWidth;
+
+        frustumPlanes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+
+
+        // Debug.developerConsoleVisible = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        //Gizmos.matrix = Camera.main.transform.localToWorldMatrix;
+        //Gizmos.DrawFrustum(new Vector3(0,0,Camera.main.nearClipPlane), Camera.main.)
     }
 
     private void LateUpdate()
-    {               
-        if(Screen.orientation == ScreenOrientation.LandscapeLeft||Screen.orientation == ScreenOrientation.LandscapeRight && !shouldMoveToTopDownView)
+    {
+
+        bool Intersects = GeometryUtility.TestPlanesAABB(frustumPlanes, myTargets[0].bounds);
+
+        Debug.Log(Intersects);
+
+
+        if (Screen.orientation == ScreenOrientation.LandscapeLeft||Screen.orientation == ScreenOrientation.LandscapeRight && !shouldMoveToTopDownView)
         {
-            if(myTargets[0] != null)
+            if (myTargets[0] != null && !shouldMoveToTopDownView)
             {
-                Camera.main.orthographicSize = ((myTargets[0].bounds.size.z / Camera.main.aspect) + myZoomPaddingLandscape);
+                targetCameraWidth = (Mathf.Abs(Mathf.Sqrt(Mathf.Pow(myTargets[0].bounds.size.x, 2) + Mathf.Pow(myTargets[0].bounds.size.z, 2))) / 1.86f);
+
+                Camera.main.orthographicSize = targetCameraWidth / Camera.main.aspect + myZoomPaddingLandscape;
+
+                Debug.Log(targetCameraWidth);
             }
-            else
-            {
-                Camera.main.orthographicSize = ((Camera.main.aspect) + myZoomPaddingLandscape);
-            }
-            //Debug.Log("In landscape mode");
+           
         }
         else if (Screen.orientation == ScreenOrientation.Portrait && !shouldMoveToTopDownView)
         {
             if (myTargets[0] != null)
             {
-                Camera.main.orthographicSize = ((myTargets[0].bounds.size.z / Camera.main.aspect) - myZoomPaddingPortrait);
-            }
-            else
-            {
-                Camera.main.orthographicSize = ((Camera.main.aspect) + myZoomPaddingPortrait);
-            }
+                Debug.Log(targetCameraWidth);
+
+                targetCameraWidth = (Mathf.Abs(Mathf.Sqrt(Mathf.Pow(myTargets[0].bounds.size.x, 2) + Mathf.Pow(myTargets[0].bounds.size.z, 2)))/1.86f);
+
+
+                Camera.main.orthographicSize = targetCameraWidth / Camera.main.aspect + myZoomPaddingPortrait;
+            }            
         }
     }
 
@@ -72,18 +102,25 @@ public class CameraController : MonoBehaviour
     {
         if(shouldMoveToTopDownView)
         {
-            transform.position = Vector3.Lerp(transform.position, new Vector3(myWorldCenterPostion.x, transform.position.y, myWorldCenterPostion.z), Time.deltaTime* transitionSpeed);
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(90, 0, 0), Time.deltaTime* transitionSpeed);
-            Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, myZoomPaddingTopDown, Time.deltaTime * transitionSpeed);
+            if (!Input.GetMouseButtonDown(0))
+            {
+                transform.position = Vector3.Lerp(transform.position, new Vector3(myWorldCenterPostion.x, transform.position.y, myWorldCenterPostion.z), Time.deltaTime * transitionSpeed);
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(90, 0, 0), Time.deltaTime * transitionSpeed);                              
+            }
         }
 
         if (!shouldMoveToTopDownView)
         {
-            transform.position = Vector3.Lerp(transform.position, originalPosition, Time.deltaTime * transitionSpeed);
-            transform.rotation = Quaternion.Lerp(transform.rotation, originalRotation, Time.deltaTime * transitionSpeed);
+            if(!Input.GetMouseButtonDown(0))
+            {
+                transform.position = Vector3.Lerp(transform.position, originalPosition, Time.deltaTime * transitionSpeed);
+
+                transform.rotation = Quaternion.Lerp(transform.rotation, originalRotation, Time.deltaTime * transitionSpeed);             
+            }
+
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Input.touchCount>1)
         {
             MoveToTopDownView();
         }
---
+++ Assets/Scripts/CameraShaker.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShaker : MonoBehaviour
+{
+
+    public Vector3 amount = new Vector3(1f, 1f, 0);
+
+    public float duration = 1;
+
+    public float speed = 10;
+
+    public AnimationCurve curve = AnimationCurve.EaseInOut(0, 1, 1, 0);
+
+    public Transform camera;
+
+    float time = 0;
+    Vector3 lastPos;
+    Vector3 nextPos;
+
+    
+    public void Shake()
+    {
+        ResetCamera();
+        time = duration;
+    }
+
+    private void LateUpdate()
+    {
+        if(time > 0)
+        {
+            time -= Time.deltaTime;
+            if(time > 0)
+            {
+                nextPos = (Mathf.PerlinNoise(time * speed, time * speed * 2)-0.5f) * amount.x * camera.right * curve.Evaluate(1f-time/duration) +
+                    (Mathf.PerlinNoise(time * speed * 2, time * speed) -0.5f) * amount.y * transform.up * curve.Evaluate(1f - time / duration);
+
+                camera.Translate(nextPos - lastPos);
+                lastPos = nextPos;
+            }
+            else
+            {
+                ResetCamera();
+            }
+        }
+    }
+
+    public void StopShake()
+    {
+        time = 0;
+    }
+
+    void ResetCamera()
+    {
+        lastPos = nextPos = Vector3.zero;
+        camera.localPosition = Vector3.zero;
+    }
+
+}
