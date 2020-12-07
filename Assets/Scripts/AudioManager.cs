e258f6aa22fc7a8078716ae6614e7bf2941b0ae7
TelluriumTrioxide <71002915+TelluriumTrioxide@users.noreply.github.com>
2020-12-07 16:37:37 +0100

    Gjorde en audio manager där man kan lägga in nya ljud och göra play-funktioner för. Fråga mig om hur man gör

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+   public static AudioManager ourInstance;
+
+   [SerializeField] private AudioSource[] myMusic;
+   [SerializeField] private AudioSource myTilePlacementSound;
+
+   private List<AudioSource> myAudioSources = new List<AudioSource>();
+   private List<bool> myAudioWasPlaying = new List<bool>();
+   //private float myLastTimeScale;
+
+   private void Awake()
+   {
+
+      if (ourInstance == null)
+      {
+         ourInstance = this;
+      }
+      else
+      {
+         Destroy(this);
+      }
+      foreach (Transform child in transform)
+      {
+         if (child != transform)
+         {
+            AudioSource audioSource = child.GetComponent<AudioSource>();
+            myAudioSources.Add(audioSource);
+            myAudioWasPlaying.Add(false);
+         }
+      }
+      //myLastTimeScale = Time.timeScale;
+   }
+   public void VolumeSettings(float aVolume)
+   {
+      AudioListener.volume = aVolume;
+   }
+   //private void CheckPause()
+   //{
+
+   //}
+   private void Update()
+   {
+      //CheckPause();   
+   }
+
+   //Play-metoder
+   public void PlayMusic()
+   {
+      switch(SceneManager.GetActiveScene().buildIndex)
+      {
+         default:
+            myMusic[0].Play();
+            break;
+      }
+   }
+
+}
