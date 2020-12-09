92130820320581825dd32e8ae4f336c2756fbfb2
VictorPetersson3 <victor.petersson@studerande.thegameassembly.se>
2020-12-09 16:26:42 +0100

    Fixat så att levels som är låsta har en annan färg

--- Assets/UI/Scripts/Options/Sc_OptionsMenu.cs
+++ Assets/UI/Scripts/Options/Sc_OptionsMenu.cs
@@ -16,12 +16,12 @@ public class Sc_OptionsMenu : MonoBehaviour
     {
         myOptionsGameObject.SetActive(true);
         LeanTween.alphaCanvas(myOptionsCanvasGroup, 1.0f, 0.50f).setEase(LeanTweenType.easeInExpo);
-        LeanTween.scale(myOptionsTransform, new Vector3(1.0f, 1.0f, 1.0f), 0.50f).setEase(LeanTweenType.easeInExpo);
+        LeanTween.scale(myOptionsTransform, new Vector3(1.0f, 1.0f, 1.0f), 0.30f).setEase(LeanTweenType.easeInExpo);
     }
     public void HideOptionsMenu()
     {
         LeanTween.alphaCanvas(myOptionsCanvasGroup, 0.0f, 0.50f).setEase(LeanTweenType.easeOutExpo);
-        LeanTween.scale(myOptionsTransform, new Vector3(0.0f, 0.0f, 0.0f), 0.50f).setEase(LeanTweenType.easeOutExpo);
+        LeanTween.scale(myOptionsTransform, new Vector3(0.0f, 0.0f, 0.0f), 0.30f).setEase(LeanTweenType.easeOutExpo);
         Invoke("DeactivateObject", 0.50f);
     }
     private void DeactivateObject()
