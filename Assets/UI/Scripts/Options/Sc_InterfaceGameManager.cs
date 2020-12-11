3cac4ff59e5dc4f58c7b08a6f06662944253e6e1
VictorPetersson3 <victor.petersson@studerande.thegameassembly.se>
2020-12-11 17:14:03 +0100

    Making so the Gamemanager holds the Options data

---
+++ Assets/UI/Scripts/Options/Sc_InterfaceGameManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sc_InterfaceGameManager : MonoBehaviour
+{
+    public void ShowOptionsMenu()
+    {
+        GameManager.globalInstance.ShowOptionsMenu();
+    }
+}
